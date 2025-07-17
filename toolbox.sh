7e996754f1fabd1c9f722fc2d66c3b18bcefd706
luizansounds <elatetunic@gmail.com>
2025-07-17 02:34:23 -0300

    first commit

---
+++ toolbox.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/bash
+
+repo_name='luizansounds-repo'
+repo="${USER}@${ARKANE_REPO}${repo_name}/os/x86_64/"
+
+if [[ $1 == 'add' ]]; then
+    mv ./*/*.pkg.tar.zst ./x86_64/
+    mv ./*/*.pkg.tar.zst.sig ./x86_64/
+	repo-add $repo_name.db.tar.zst ./*/*pkg.tar.zst
+	rm -rf $repo_name.db
+	rm -rf $repo_name.files
+	mv ./$repo_name.db.tar.zst $repo_name.db
+	mv ./$repo_name.files.tar.zst $repo_name.files
+	
+elif [[ $1 == 'build' ]]; then
+	BASE_DIR=$(pwd)
+
+	for dir in *;
+	do
+		if [ -d "$dir" ];
+		then
+			git clone https://github.com/luizansounds/pkgbuild.git
+			cd pkgbuild
+			rm -rf ./86_64/*
+			makepkg -fcd --sign --key $(git config user.email)
+			cd $BASE_DIR
+		fi
+	done
+
+elif [[ $1 == 'clear' ]]; then
+	rm ./*.old
+elif [[ $1 == 'push' ]]; then
+
+	scp ./$repo_name.* $repo
+	scp ./*/*.pkg.tar.zst $repo
+	scp ./*/*.pkg.tar.zst.sig $repo
+else
+	printf "No valid parameter provided\n"
+fi
